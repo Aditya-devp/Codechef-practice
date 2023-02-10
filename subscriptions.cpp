282f69f96aae6df445d5aef3a4eed8522abb8f5c
Aditya Singh <singhaditya6774@gmail.com>
2023-02-10 21:25:33 +0530

    Create subscriptions.cpp

---
+++ subscriptions.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int test;
+cin>>test;
+while(test--){
+    int a,b;
+    cin>>a>>b;
+    if(a<=6){
+        cout<<b<<endl;
+    }
+    else if(a%6==0){
+        cout<<(a/6)*b<<endl;
+    }
+    else{
+        cout<<((a/6)+1)*b<<endl;
+    }
+}
+	return 0;
+}
