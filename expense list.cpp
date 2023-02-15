686737debed006f605ebb06e9e8e51650c746588
Aditya Singh <singhaditya6774@gmail.com>
2023-02-15 14:36:14 +0530

    Create expense list.cpp

---
+++ expense list.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int test;
+cin>>test;
+while(test--){
+    int a,b,c;
+    cin>>a>>b;
+   c=pow(2,b-a);
+   cout<<c<<endl;
+ 
+}
+	return 0;
+}
