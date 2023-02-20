9ae7a77fe2ef057d73bc9d1d9758fdac6fbe304e
Aditya Singh <singhaditya6774@gmail.com>
2023-02-20 22:47:16 +0530

    Create chef and wire frame.cpp

---
+++ chef and wire frame.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int test; 
+	cin>>test; 
+	while(test--){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    cout<<(a*2+b*2)*c<<endl;
+	    
+	}
+		return 0;
+}
