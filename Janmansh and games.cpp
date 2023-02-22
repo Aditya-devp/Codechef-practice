b8200bd35c30a713f90b6d54d79c781fa821279e
Aditya Singh <singhaditya6774@gmail.com>
2023-02-22 14:09:48 +0530

    Create Janmansh and games.cpp

---
+++ Janmansh and games.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int test;
+cin>>test;
+while(test--){
+    int a,b;
+    cin>>a>>b;
+    if((a+b)%2==0){
+        cout<<"Janmansh"<<endl;
+    }
+    else{
+        cout<<"Jay"<<endl;
+    }
+}
+	return 0;
+}
