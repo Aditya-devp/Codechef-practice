9414deec715d9cfb56fe7e9063be01072dc0e9f2
Aditya Singh <singhaditya6774@gmail.com>
2023-02-24 14:41:35 +0530

    Create primality test.cpp

---
+++ primality test.cpp
@@ -0,0 +1,31 @@
+
+
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) { 
+        return false;
+    }
+    for (int i = 2; i <= n / i; i++) { 
+        if (n % i == 0) { 
+            return false;
+        }
+    }
+    return true; 
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        if (isPrime(n)) {
+            cout << "yes"<<endl;
+        } else {
+            cout << "no"<<endl;
+        }
+    }
+    return 0;
+}
