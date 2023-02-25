d993331a651a479d9cd6ce83da73d42c79a5354a
Aditya Singh <singhaditya6774@gmail.com>
2023-02-25 21:56:33 +0530

    Create time complexity.cpp

---
+++ time complexity.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int test;
+cin>>test;
+while(test--){
+    int a,b;
+    cin>>a>>b;
+    if(a>b){
+        cout<<"Yes"<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+    }
+}
+	return 0;
+}
