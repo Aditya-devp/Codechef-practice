429736ac231e52a9fcf0fe6cae5aaa1412484e71
Aditya Singh <singhaditya6774@gmail.com>
2023-03-05 22:56:21 +0530

    Create ezio and guards.cpp

---
+++ ezio and guards.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ int test;
+ cin>>test;
+ while(test--){
+     int a,b;
+     cin>>a>>b;
+     if(a>=b){
+         cout<<"yes"<<endl;
+     }
+     else{
+         cout<<"No"<<endl;
+     }
+ }
+	return 0;
+}
