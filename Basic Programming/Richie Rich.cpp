8827f34e4cf8c06157f1651c330f7f4581d238b6
Aditya Singh <singhaditya6774@gmail.com>
2023-03-22 13:59:34 +0530

    Create Richie Rich.cpp

---
+++ Basic Programming/Richie Rich.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int test;
+	cin>>test;
+	while(test--){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    cout<<(b-a)/c<<endl;
+	}
+	return 0;
+}
